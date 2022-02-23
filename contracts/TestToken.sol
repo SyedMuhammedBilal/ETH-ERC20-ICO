a3e240d9ff76d95c777342e38c72e7a08e6cbdbc
Syed Muhammed Bilal <SyedMuhammedBilal@vtechs-MacBook-Pro.local>
2022-02-24 00:10:41 +0500

    passed test and necessary ERC20 functions

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -28,6 +28,10 @@ contract TestToken is ITestToken {
         balanceOf[msg.sender] = _totalSupply;
     }
 
+    /**
+       * @param _to address The address which you want to transfer to
+       * @param _value uint256 The amoung of token to be spent
+    */
     function transfer(address _to, uint256 _value) public override returns (bool) {
         require(balanceOf[msg.sender] >= _value, "Not enough balance");
 
@@ -38,12 +42,21 @@ contract TestToken is ITestToken {
         return true;
     }
 
+    /**
+        * @param _spender address The address which will spend the funds
+        * @param _value uint256 The amoung of token to be spent
+    */
     function approve(address _spender, uint256 _value) public override returns (bool) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
+    /**
+        * @param _from address The address which you want to send tokens from
+        * @param _to address The address which you want to transfer to
+        * @param _value uint256 The amount of tokens to be transferred
+    */
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);   
